1b00121184c73910707602f3fcc2c38e4db4ef47
Eugene Mutai <eugene.mutai@andela.com>
2015-10-07 23:33:51 +0300

    Fixed postgreSQL connection config

--- server/config/db-connect.js
+++ server/config/db-connect.js
@@ -1,7 +1,9 @@
 var Seq = require('sequelize'),
-    sequelize = new Seq('vvida', 'postgres', 'root', {
-        host: 'localhost',
-        dialect: 'postgres'
-    });
+  sequelize = new Seq('vvida', 'kn9ts', 'root', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432
+  });
 
+sequelize.sync();
 module.exports = sequelize;
