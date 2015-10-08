da9a8f78de8e02c2a76c799d64bc9336d033ddda
Eugene Mutai <eugenemutai@gmail.com>
2015-10-08 02:31:20 +0200

    Fixed wrong URL port being loaded by the selenium browser instance, 3000 => 3880

--- tests/e2e/end.spec.js
+++ tests/e2e/end.spec.js
@@ -1,7 +1,7 @@
 describe('Todolist app', function() {
 
   it('The app title is \'todos\'', function() {
-    browser.get('http://localhost:3000/');
+    browser.get('http://localhost:3880/');
     expect(element(by.id('app-title')).getText()).toEqual('todos');
   });
 
