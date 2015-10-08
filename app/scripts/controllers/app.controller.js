425ae3f35acbd94e712a23e35ba2ec2bcb9cde17
Eugene Mutai <eugenemutai@gmail.com>
2015-10-08 23:50:31 +0300

    Refactored the app's main controller, moving logic to todo.comtroller

--- app/scripts/controllers/app.controller.js
+++ app/scripts/controllers/app.controller.js
@@ -1,16 +1,6 @@
 // The main app controller
-var AppController = function($scope, $rootScope, listing) {
-  $scope.newTask = undefined;
+var AppController = function($scope, $rootScope, Todo) {
   $scope.appTitle = 'Todo app';
-
-  $scope.addTask = function(form) {
-    console.log('THE FORM', $scope.newTask);
-
-    // Tell the other controllers a new task has been done
-    // $rootScope.$broadcast('new_task_has_been_added', $scope.new_task);
-    listing.add($scope.newTask);
-    $scope.newTask = undefined;
-  };
 };
 
 module.exports = AppController;
