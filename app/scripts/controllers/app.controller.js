33e0f49d132577584cd9c39e1168043a2977b82c
Eugene Mutai <eugenemutai@gmail.com>
2015-10-09 15:44:42 +0300

    Added a few more Author object for testing purposes

--- app/scripts/controllers/app.controller.js
+++ app/scripts/controllers/app.controller.js
@@ -1,6 +1,14 @@
 // The main app controller
 var AppController = function($scope) {
   $scope.appTitle = 'Todo app';
+  $scope.appAuthor = {
+    name: 'Eugene Mutai',
+    email: 'eugenemutai@gmail.com',
+    githubHandler: '@kn9ts',
+    twitterHandler: '@kn9ts'
+  };
+
+  console.log($scope.appTitle);
 };
 
 module.exports = AppController;
