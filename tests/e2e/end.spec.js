9af701288593651c8a4b0a07749b3d08ef113800
Eugene Mutai <eugenemutai@gmail.com>
2015-10-10 13:33:22 +0300

    Refactored end 2 end testing configurations to mock example end to end test

--- tests/e2e/end.spec.js
+++ tests/e2e/end.spec.js
@@ -1,8 +1,13 @@
-describe('Todolist app', function() {
+describe('Todo app intergration/E2E testing', function() {
+
+  // start at root before every test is run
+  beforeEach(function() {
+    browser().navigateTo('/#/');
+  });
 
   it('The app title is \'todos\'', function() {
-    browser.get('http://localhost:3880/');
-    expect(element(by.id('app-title')).getText()).toEqual('todos');
+    // browser.get('http://localhost:3880/');
+    expect(element(by.id('app-title')).getText()).toEqual('Todo app');
   });
 
 });
