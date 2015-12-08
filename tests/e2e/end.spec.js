8c57f6f8b5fa68790a27a37fae4cc2d942134c4f
Eugene Mutai <eugenemutai@gmail.com>
2015-12-08 11:53:20 +0300

    [refactor] corrected e2e test decription

--- tests/e2e/end.spec.js
+++ tests/e2e/end.spec.js
@@ -4,7 +4,7 @@ describe('Todolist app', function() {
     browser.get('http://localhost:3000');
   });
 
-  it('The app title is \'todos\'', function() {
+  it('The app title is \'Todo app\'', function() {
     expect(element(by.id('app-title')).getText()).toEqual('Todo app');
   });
 
